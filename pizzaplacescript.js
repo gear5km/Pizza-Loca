1a7434e62102fccae387be2874ff338285b57ca1
gear5km <https://github.com/gear5km/Project-Reality-Arcade/tree/v0.1>
2020-03-03 15:54:34 +0300

    0.3

--- pizzaplacescript.js
+++ pizzaplacescript.js
@@ -1,14 +1,22 @@
 
-function pizzaToppings(toppings, price){
-    this.toppings=toppings;
+function pizzaToppings(pizzaName, price){
+    this.name=name;
     this.price=price;
 }
 
-var chicken= new pizzaToppings (chicken, 500);
-var pepperoni= new pizzaToppings (pepperoni, 400);
-var hawaian= new pizzaToppings (hawaian, 450);
+var chicken= new pizzaToppings ("chicken", 500);
+var pepperoni= new pizzaToppings ("pepperoni", 400);
+var hawaian= new pizzaToppings ("hawaian", 450);
+var beefOnion = new pizzaToppings ("beefOnion", 500);
+var steakCheese = new pizzaToppings ("steakCheese", 550);
+var oliveMushroom = new pizzaToppings ("oliveMushroom", 400);
+var somethingMeaty= new pizzaToppings ("somethingMeaty", 450);
+var fetuchini= new pizzaToppings ("fetuchini", 550);
 
 
+var pizzaSelection= ["1","2","3","4","5","6","7","8"]
+
+pizzaSelection["1"]=chicken;
 
 
 function pizzaSize (size, price) {
